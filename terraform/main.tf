f4598f029136a08ade7ffd4119625ad88607e337
Chocopyke <Vu.tuanson159@gmail.com>
2024-10-24 22:10:42 +0700

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
 module "vpc" {
   source = "./modules/vpc"
+  
+  region = "us-east-1"
+  proj_name = "testing"
+  environment = "terraform"
+
+  vpc_cidr = "10.10.0.0/16"
+  public_subnet_cidr = "10.10.10.0/24"
+  private_subnet_cidr = "10.10.20.0/24"
 }
